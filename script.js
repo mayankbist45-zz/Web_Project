a20750e043f3741eb5aaff88b7c9f7687c46b59d
Mayank Bist <mayankbist45@gmail.com>
2020-11-06 15:53:37 +0530

    ui improvement next

--- script.js
+++ script.js
@@ -13,18 +13,6 @@ anime.timeline().add({
   duration: 950,
   delay: (el, i) => 70 * i,
 });
-textWrapper = document.querySelector(".ml6 .letters");
-textWrapper.innerHTML = textWrapper.textContent.replace(
-  /\S/g,
-  "<span class='letter'>$&</span>"
-);
-anime.timeline({ loop: false }).add({
-  targets: ".ml6 .letter",
-  translateY: ["1.1em", 0],
-  translateZ: 0,
-  duration: 750,
-  delay: (el, i) => 50 * i,
-});
 
 var quotes = [
   "“The secret of getting ahead is getting started.” – Mark Twain",
